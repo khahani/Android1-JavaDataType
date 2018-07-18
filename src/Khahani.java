100faf6138d49e439c398b5d8cce7dcb46bf89a7
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-18 03:36:25 -0700

    define various variable in java

--- src/Khahani.java
+++ src/Khahani.java
@@ -1,5 +1,28 @@
+/**
+ * Developer and Teacher: Khahani.com
+ * Course: Android 1
+ */
+
 public class Khahani {
     public static void main(String[] args) {
+
+        // template for writing variable in java
+        /*
+                [DataType] [variableName];
+                [DataType] [variableName] = [Initial Value];
+                [DataType] [variableName], [variableName];
+         */
+
+        byte b = 127;
+        short sh = 2300;
+        int i = 35;
+        long l = 152000000L;
+
+        float f = 14.5f;
+        double d = 12.15312111;
+
+        char c = 'a';
+        String name = "Mohammadreza Khahani";
 
     }
 }
